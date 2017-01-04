becabbc458ed393e0c4be049e77663436ea7b0eb
Pavel Rojtberg <rojtberg@gmail.com>
2017-01-04 02:28:04 +0100

    add 1.10 api docs

---
+++ api/1.10/class_ogre_1_1_r_t_shader_1_1_assignment_atom.js
@@ -0,0 +1,18 @@
+var class_ogre_1_1_r_t_shader_1_1_assignment_atom =
+[
+    [ "OperandVector", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a709fbe496a60e5c9568461c236b4fa26", null ],
+    [ "AssignmentAtom", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a9fcaeaf8c992716d2313438ae76e3e13", null ],
+    [ "AssignmentAtom", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#ad730369c7f4e96ffbba4584383a6b7c3", null ],
+    [ "writeSourceCode", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#ac22b068e0445c466059b87ec534bf5e2", null ],
+    [ "getFunctionAtomType", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a147a812c02bc7ac1186f38cb15195f30", null ],
+    [ "getOperandList", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a9e9e5dd69fd84e4c67b89605bc608f4e", null ],
+    [ "pushOperand", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#afc5bce23c96684eee4cba52bfe778d51", null ],
+    [ "getFunctionName", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#ac59d6c18ad1b47e6748b173ccd8c9d0f", null ],
+    [ "getReturnType", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a7025e33c39fe3126043a769321118b36", null ],
+    [ "operator==", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a39c8acbda74217791c7f4aa847c776f1", null ],
+    [ "operator!=", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a1e48a2cb0e3f18ff3245ea12ee8542f0", null ],
+    [ "operator<", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a59d5e7a792b52124ccff625e133d3ccd", null ],
+    [ "getGroupExecutionOrder", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#ab061ac5ec329a636ee0977d0e7364ff5", null ],
+    [ "getInternalExecutionOrder", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#a157f51d4427fab33143f7156be05ba60", null ],
+    [ "Type", "class_ogre_1_1_r_t_shader_1_1_assignment_atom.html#ab8558a732b4b321151a6fba6c0e686f9", null ]
+];
---
+++ api/1.10/class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.js
@@ -0,0 +1,27 @@
+var class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting =
+[
+    [ "NormalMapSpace", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a759726c217598d08d109343b48201b0c", [
+      [ "NMS_TANGENT", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a759726c217598d08d109343b48201b0ca2114578ed70122ae52cedcbe5484db30", null ]
+    ] ],
+    [ "PBRLighting", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a36c90042ab5a496e00e1656d9be16d12", null ],
+    [ "getType", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a89068f0449f52630d52f1214f101e7c4", null ],
+    [ "updateGpuProgramsParams", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a81fc0e7b923ad214a662ba59fdc2dbdf", null ],
+    [ "copyFrom", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a5fc35d519c21cb51e3ad000667afb814", null ],
+    [ "preAddToRenderState", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a5a5a619c0027f1912c7e28ab195ffee6", null ],
+    [ "setTexCoordIndex", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#aa24dace83f8da84c997d09216978acab", null ],
+    [ "getTexCoordIndex", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#aa9429513aa7ce4e112139fb949d0ab8c", null ],
+    [ "setNormalMapTextureName", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a10fb7f8849b73149b01b5c774d0bc115", null ],
+    [ "setEnvironmentMapTextureName", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a55977ac1822bcda0ba38aec0d5fc98c5", null ],
+    [ "getNormalMapTextureName", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#ae77c7a2a9b7bdd6e6690278af5bc78b0", null ],
+    [ "setNormalMapFiltering", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a76cdc0302da0d0ab4ffe61d08cc7452b", null ],
+    [ "getNormalMapFiltering", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a1e8eceb6f02b0961eb70939237562642", null ],
+    [ "setNormalMapAnisotropy", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#ab344d3b6130ce3f436b91e740cf716ec", null ],
+    [ "getNormalMapAnisotropy", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a134299ab9bb7ec7271dace1cf5f18a14", null ],
+    [ "setNormalMapMipBias", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#af9b1253896c285746396fabdf31aaf54", null ],
+    [ "getNormalMapMipBias", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a32907118081a01322ab9dfeab09a3bf4", null ],
+    [ "getExecutionOrder", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#aae139ed545351b222000e21c45ea44ef", null ],
+    [ "createCpuSubPrograms", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a4b43c261946926ebbc6456deccc6697a", null ],
+    [ "getAccessor", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#aa73560a3f1b9117cb8b438867848c255", null ],
+    [ "getAccessor", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#a4023f69bcd64cf1f4f2e68d23cd627c3", null ],
+    [ "Type", "class_ogre_1_1_r_t_shader_1_1_p_b_r_lighting.html#af1f256dbec16745070b1a8103ec12076", null ]
+];
